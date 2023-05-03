80dd5006c5acb0f7ca286308dd108f51ff9566cd
Chase Morgan <chsmorg@aol.com>
2023-05-03 16:15:36 -0500

    connection to database

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose')
 
-const taskModel = require('./taskModel')
-const userModel = require('./userModel')
+// const taskModel = require('./taskModel')
+// const userModel = require('./userModel')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+mongoose.connect(process.env.DB)
+.then(()=>{
+  console.log('Connected to database')
+})
+.catch((err)=>{
+    console.error(err)
+})
+
+
