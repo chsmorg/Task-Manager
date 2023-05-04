3715af5175cc0be17337a2e46da3d8c3c8f06be5
ashtonhess <ashton.hess@yahoo.com>
2023-05-04 16:13:16 -0500

    Adding header component.

--- Task-Manager/src/app/header/header.component.ts
+++ Task-Manager/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(private router: Router) { }
+  goToCreateTask() {
+    this.router.navigate(['/task-create']);
+  }
+  goToListTasks() {
+    this.router.navigate(['/task-list']);
+  }
+ logout(){
+  
+ }
 }
