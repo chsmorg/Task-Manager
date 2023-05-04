42d2557254e34676138369f8a8877357088a0aa3
ashtonhess <ashton.hess@yahoo.com>
2023-05-03 20:38:47 -0500

    Adding routing for login and register pages.

--- Task-Manager/src/app/app-routing.module.ts
+++ Task-Manager/src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+const routes: Routes = [
+  {
+    path:'', component: LoginComponent
+  },
+  {
+    path:'register', component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
+
+
+// import { NgModule } from "@angular/core";
+// import { RouterModule, Routes } from "@angular/router";
+// import { PostCreateComponent } from "./post/post-create/post-create.component";
+// import { PostListComponent } from "./post/post-create/post-list/post-list.component";
+
+// const routes: Routes = [
+//   {
+//     path:'', component: PostListComponent
+//   },
+//   {
+//     path:'create',component: PostCreateComponent
+//   }
+// ]
+
+// @NgModule({
+//   imports:[RouterModule.forRoot(routes)],
+//   exports:[RouterModule]
+// })
+
+// export class AppRoutingModule{
+
+// }
